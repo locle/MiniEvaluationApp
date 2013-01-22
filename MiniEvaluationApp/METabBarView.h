b79bb6c43d51dcb835fe864e9fd5ecc6c1b9a014
tanduynguyen <tanduy.nguyen@2359media.us>
2013-01-22 17:45:22 +0700

    implemented Pull to refresh, Custom tab bar, Staff View as design

---
+++ MiniEvaluationApp/METabBarView.h
@@ -0,0 +1,13 @@
+//
+//  METabBarView.h
+//  MiniEvaluationApp
+//
+//  Created by viet on 1/22/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface METabBarView : UIView
+
+@end
---
+++ MiniEvaluationApp/METabBarView.m
@@ -0,0 +1,31 @@
+//
+//  METabBarView.m
+//  MiniEvaluationApp
+//
+//  Created by viet on 1/22/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import "METabBarView.h"
+
+@implementation METabBarView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
